ee5e497513f645a5d51ca00c0e0da7e6ec4f9175
Dustin <dustinjlindquist@gmail.com>
2015-03-01 13:43:39 -0600

    sweet new flows

--- js/controller/popularCtrl.js
+++ js/controller/popularCtrl.js
@@ -5,55 +5,55 @@ angular.module('app')
       text: 'Save my Facebook photos to dropbox',
       fromIcon: 'fa fa-facebook fa-lg',
       toIcon: 'fa fa-dropbox fa-lg',
-      up: 340,
-      down: 40
+      up: 486,
+      down: 15
     },
     {
-      text: 'Save my Facebook photos to dropbox',
-      fromIcon: 'fa fa-facebook fa-lg',
+      text: 'Order Uber to next Google Calendar Event',
+      fromIcon: 'fa fa-taxi fa-lg',
+      toIcon: 'fa fa-calendar fa-lg',
+      up: 401,
+      down: 34
+    },
+    {
+      text: 'Create Wordle from Twitter Tweets containing #Hashtag',
+      fromIcon: 'fa fa-twitter fa-lg',
+      toIcon: 'fa fa-cloud fa-lg',
+      up: 375,
+      down: 4
+    },
+    {
+      text: 'Buy BestBuy item with Coinbase',
+      fromIcon: 'fa fa-shopping-cart fa-lg',
+      toIcon: 'fa fa-usd fa-lg',
+      up: 360,
+      down: 10
+    },
+    {
+      text: 'Save my Instagram photos to dropbox',
+      fromIcon: 'fa fa-instagram fa-lg',
+      toIcon: 'fa fa-dropbox fa-lg',
+      up: 351,
+      down: 43
+    },
+    {
+      text: 'Save my Google+ photos to dropbox',
+      fromIcon: 'fa fa-google-plus fa-lg',
       toIcon: 'fa fa-dropbox fa-lg',
       up: 340,
       down: 40
     },
     {
-      text: 'Save my Facebook photos to dropbox',
-      fromIcon: 'fa fa-facebook fa-lg',
-      toIcon: 'fa fa-dropbox fa-lg',
+      text: 'Email me contact infor for upcoming meetings',
+      fromIcon: 'fa fa-envelope fa-lg',
+      toIcon: 'fa fa-user fa-lg',
       up: 340,
       down: 40
     },
     {
-      text: 'Save my Facebook photos to dropbox',
-      fromIcon: 'fa fa-facebook fa-lg',
-      toIcon: 'fa fa-dropbox fa-lg',
-      up: 340,
-      down: 40
-    },
-    {
-      text: 'Save my Facebook photos to dropbox',
-      fromIcon: 'fa fa-facebook fa-lg',
-      toIcon: 'fa fa-dropbox fa-lg',
-      up: 340,
-      down: 40
-    },
-    {
-      text: 'Save my Facebook photos to dropbox',
-      fromIcon: 'fa fa-facebook fa-lg',
-      toIcon: 'fa fa-dropbox fa-lg',
-      up: 340,
-      down: 40
-    },
-    {
-      text: 'Save my Facebook photos to dropbox',
-      fromIcon: 'fa fa-facebook fa-lg',
-      toIcon: 'fa fa-dropbox fa-lg',
-      up: 340,
-      down: 40
-    },
-    {
-      text: 'Save my Facebook photos to dropbox',
-      fromIcon: 'fa fa-facebook fa-lg',
-      toIcon: 'fa fa-dropbox fa-lg',
+      text: 'Public transport in my area',
+      fromIcon: 'fa fa-train fa-lg',
+      toIcon: 'fa fa-globe fa-lg',
       up: 340,
       down: 40
     },
